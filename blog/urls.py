86ec6d185b18e4a23176c0e3c0178f5636cdadb4
commieloliqq <commieloliqq@gmail.com>
2023-11-30 17:03:36 +0600

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('mytest', views.my_test, name='my_test')
+    # path('mytest', views.my_test, name='my_test'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
@@ -9,5 +9,10 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
-def my_test(request):
-    return render(request, 'blog/test.html', {})
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+
+# def my_test(request):
+#     return render(request, 'blog/test.html', {})
